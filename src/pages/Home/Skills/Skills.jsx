e32e7011237ff23548906c4d445019f65cd84c7a
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-07-14 23:48:56 +0600

    skills added

--- src/pages/Home/Skills/Skills.jsx
+++ src/pages/Home/Skills/Skills.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import { FaBootstrap, FaCss3Alt, FaGithubAlt, FaHtml5, FaJs, FaNodeJs, FaReact } from "react-icons/fa";
+import { SiMongodb, SiTailwindcss } from "react-icons/si";
 
 const Skills = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="bg-gray-800 py-10 px-5 lg:px-40 font-serif">
+      <h3 className="text-xl font-semibold text-center mb-5">My Skills</h3>
+      <div className="flex items-center space-x-6 mb-4 text-9xl text-info p-4">
+        <FaHtml5/>
+        <FaCss3Alt />
+        <FaBootstrap />
+        <SiTailwindcss />
+        <FaJs></FaJs>
+        <FaReact />
+        <FaNodeJs />
+        <SiMongodb></SiMongodb> <br />
+        <FaGithubAlt />
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
